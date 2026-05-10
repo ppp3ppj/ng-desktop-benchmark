7fcb553cc4e9d14ee100c7ed2bd1480d6913b7c8
ppp3ppj <ppp.3ppj@gmail.com>
2026-05-10 15:21:16 +0700

    Setup daisy ui and tailwindcss

--- ng-electron/src/app/app.ts
+++ ng-electron/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
   protected readonly title = signal('ng-electron');
